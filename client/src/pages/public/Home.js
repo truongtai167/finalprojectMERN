7f03f74a2cdce5160b97b49acaa3874e023b7202
tuyendt0112 <dangthanhtuyen0112@gmail.com>
2023-11-23 17:40:24 +0700

    hot col

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -41,31 +41,12 @@ const Home = () => {
         <h3 className="text-[20px] font-semibold py-[15px] border-b-2 border-main">
           HOT PITCHES
         </h3>
-        <div className="flex flex-wrap gap-x-24 gap-y-8 mt-4 min-h-[190px]">
-          {categories
-            ?.filter((el) => el.brand.length > 0)
-            .map((el) => (
-              <div key={el._id} className="w-[396px]">
-                <div className="border flex p-4 gap-4">
-                  <img
-                    src={el?.images[0]}
-                    alt=""
-                    className="w-[144px] flex-1 h-[129px] object-cover"
-                  ></img>
-                  <div className="flex-1 text-gray-700">
-                    <h4 className="font-semibold uppercase">{el.title}</h4>
-                    <ul className="text-sm">
-                      {el?.brand?.map((item) => (
-                        <span key={item} className="flex gap-1 items-center">
-                          <IoIosArrowForward size={14}></IoIosArrowForward>
-                          <li>{item}</li>
-                        </span>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            ))}
+        <div className="flex flex-wrap gap-4 mt-4">
+          {categories?.map((el) => (
+            <div key={el._id} className="w-[396px]">
+              {el.title}
+            </div>
+          ))}
         </div>
       </div>
 
